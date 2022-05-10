1251bdf48c569efa4b897774fd6889ef02271db4
pradip-tcs <100670828+pradip9821-tcs@users.noreply.github.com>
2022-05-10 17:17:28 +0530

    Fix register

--- app/utils/database.js
+++ app/utils/database.js
@@ -4,13 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.USER_NAME, process.env.DATABASE_PASSWORD, {
     dialect: 'mysql',
     host: process.env.HOST_NAME,
-    port: 3306,
-    pool: {
-        max: 50,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+    port: 3306
 });
 
 module.exports = sequelize;
